06ca402c77df6cdd21be28d30e775b8176ea04c4
programmernizam <mdnizam12109@gmail.com>
2023-08-27 13:11:44 +0600

    Logo component created

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -1,6 +1,19 @@
 "use client";
+
+import Image from "next/image";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div>
+      <Image
+        className="hidden md:block cursor-pointer"
+        alt="Logo"
+        height={100}
+        width={100}
+        src="/images/logo.png"
+      />
+    </div>
+  );
 }
 
 export default Logo;
