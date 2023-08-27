000787353641c94ca103e934e13463d2122ebde7
programmernizam <mdnizam12109@gmail.com>
2023-08-27 12:57:27 +0600

    Starting create navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar/Navbar";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
